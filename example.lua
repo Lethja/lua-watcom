2b62710c462a4b5c49a856fafa5c3ca76c749ee2
Jason Lethbridge <lethbridgejason@gmail.com>
2025-02-27 02:36:57 +1100

    Added example.lua and removed its generation from the GitHub workflow

---
+++ example.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua
+-- This is a single line comment, the line above is a shebang for UNIX systems
+
+--[[ This is a multi-line comment. To run the script, pass it to the lua binary
+ as an argument (`LUA16.EXE EXAMPLE.LUA` on 16-bit DOS for example).
+ For full Lua language documentation visit https://www.lua.org/docs.html
+--]]
+
+hour = tonumber(os.date('%H')) -- Get the hour of day on the computers clock
+if hour < 4 or hour > 20 then  -- Convert hour into fuzzy time of day
+    timeOfDay = 'night'
+elseif hour < 9 then
+    timeOfDay = 'morning'
+elseif hour > 16 then
+    timeOfDay = 'evening'
+else
+    timeOfDay = 'day'
+end
+
+print('Good ' .. timeOfDay .. ' from ' .. _VERSION .. '.') -- Print a greeting
+io.read() -- Wait for Enter to be pressed
+print('Have a good ' .. timeOfDay .. '.') -- This message will
+os.exit() -- Exit script. Will also exit a interactive shell
