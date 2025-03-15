3e3a16d0b1e9ffc2d1c45b9b6959d1b8f004bb91
Jason Lethbridge <lethbridgejason@gmail.com>
2025-03-16 00:12:20 +1100

    Added the extended benchmark (`exbench.lua`) that currently has MD5 and SHA256 checksum tests.
    This script benchmarks MD5 and SHA256 checksum computation for files. It allows each benchmark to be skipped if an error occurs, otherwise it runs multiple iterations of the file checksum (using the entry file as a sample) to measure performance. `exbench.lua` is called by `bench.lua` to keep basic benchmarks separated from more complex and dependency-driven ones.

---
+++ demo/extra/exbench.lua
@@ -0,0 +1,41 @@
+local function benchmark_md5sum()
+    local it, s = 5, pcall(require, "md5sum")
+    local bm = BenchmarkStart("Checksum MD5", it)
+    local file = io.open(arg[0], "rb")
+
+    if not s or not file then
+        if file then file:close() end
+        print(string.format("%17s", "Skipped"))
+        return
+    end
+
+    for _ = 1, it do
+        md5_file(file)
+        file:seek("set", 0)
+    end
+    file:close()
+    BenchmarkEnd(bm)
+end
+
+local function benchmark_sha256()
+    local it, s = 5, pcall(require, "s256sum")
+    local bm = BenchmarkStart("Checksum SHA256", it)
+    local file = io.open(arg[0], "rb")
+
+    if not s or not file then
+        if file then file:close() end
+        print(string.format("%17s", "Skipped"))
+        return
+    end
+
+    for _ = 1, it do
+        sha256_file(file)
+        file:seek("set", 0)
+    end
+    file:close()
+    BenchmarkEnd(bm)
+end
+
+LIB = true
+benchmark_md5sum()
+benchmark_sha256()
