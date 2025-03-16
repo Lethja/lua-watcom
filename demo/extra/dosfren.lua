a5744e8a5c5fa534b3591e5260d73ab2b01273e5
Jason Lethbridge <lethbridgejason@gmail.com>
2025-03-17 01:05:08 +1100

    Documentation and code improvements in `dosfren.lua`.
    Improve file processing logic by copying the temporary back to the original file to retain the same system file permissions. Simplified variable naming to make the scripts filesize smaller and updated comments for clarity.

--- demo/extra/dosfren.lua
+++ demo/extra/dosfren.lua
@@ -1,20 +1,19 @@
 #!/usr/bin/env lua
 
-
 help = [[
 DOS and UNIX disagree on what a new line should be
 DOS says it's a carriage return followed by a line feed (CRLF)
 while UNIX says it's just a line feed on its own (LF).
 Many tools (Lua included) don't care while others like
-`TYPE` in DOS and Notepad in Windows will not display correctly.
+`TYPE` in DOS and Notepad in Windows will not work correctly.
 Tools like unix2dos/dos2unix convert line endings between the systems,
 however, in doing so, the CR in CRLF on the shebang line might be
-interpreted literally and prevent correct execution on UNIX systems.
+interpreted literally and prevent execution on UNIX systems.
 
 This script named "DOS friend" will convert every line ending of the files
 passed to it with CRLF with the exception of the first line when a shebang
 is present. When this occurs, the line will instead write LFCRLF so that
-both types of systems can pleasantly read and run the file.
+both system families can pleasantly read and run the file.
 ]]
 
 if #arg < 1 then
@@ -40,31 +39,46 @@ local function Shebang(inFile)
 end
 
 for i = 1, #arg do
-	local inFile, err = io.open(arg[i])
-	if not inFile then print(arg[i] .. ": " .. err) else
-		local outFile
-		outFile, err = io.open(arg[i] .. ".tmp", "wb")
-		if not outFile then
+	local iFile, err = io.open(arg[i])
+	if not iFile then print(arg[i] .. ": " .. err) else
+		local oFile
+		oFile, err = io.open(arg[i] .. ".tmp", "wb")
+		if not oFile then
 			print(arg[i] .. ".tmp: " .. err)
 		else
-			local shebang = Shebang(inFile)
+			local shebang = Shebang(iFile)
 			if shebang then
-				outFile:write(shebang)
+				oFile:write(shebang)
 				while true do
-					local line = inFile:read("*l")
+					local line = iFile:read("*l")
 					if line then
-						outFile:write(StripCarriage(line) .. CRLF)
+						oFile:write(StripCarriage(line) .. CRLF)
 					else
 						break
 					end
 				end
-				inFile:close() outFile:close()
-				os.remove(arg[i])
-				os.rename(arg[i] .. ".tmp", arg[i])
-			else
-				inFile:close() outFile:close()
-				os.remove(arg[i] .. ".tmp")
+				iFile:close() oFile:close()
+
+				--[[
+					Swap the temporary file and original file around
+					so that the original files is being copied to.
+					This allows system file permissions to remain unchanged.
+				--]]
+
+				iFile = io.open(arg[i] .. ".tmp", "rb")
+				oFile = io.open(arg[i], "wb")
+				if iFile and oFile then
+					while true do
+						local data = iFile:read(1024)
+						if not data then
+							break
+						end
+						oFile:write(data)
+					end
+					iFile:close() oFile:close()
+				end
 			end
+			os.remove(arg[i] .. ".tmp")
 		end
 	end
 end
